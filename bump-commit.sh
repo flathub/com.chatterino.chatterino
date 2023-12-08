58f21beb3493bb3c499bc841a4664dbab37acaf9
Ilya Zlobintsev <ilya.zl@protonmail.com>
2023-12-08 23:18:52 +0200

    Branch/nightly (#269)
    * Bump commit to 073192b
    
    * Bump commit to 5c55f62
    
    * Bump commit to c8e1741
    
    * Bump commit to 00642ef
    
    * Bump commit to 610394d
    
    * Bump commit to fe2b82b
    
    * Bump commit to 52a6f25
    
    * Bump commit to cd6e1c0
    
    * Bump commit to 8fd9752
    
    * Bump commit to 88bb1b4
    
    * Bump commit to d6ef48d
    
    * Bump commit to 517382e
    
    * Bump commit to 20bdfae
    
    * Bump commit to 169f419
    
    * Bump commit to 421497c
    
    * Bump commit to ec4ec79
    
    * Bump commit to cb0ab90
    
    * Bump commit to 32499f0
    
    * Bump commit to bd58a55
    
    * Bump commit to 6338d09
    
    * Bump commit to 6d2605d
    
    * Bump commit to 4d0e4c1
    
    * Bump commit to 9c9fa86
    
    * Bump commit to 6427184
    
    * Bump commit to 0136d9b
    
    * Bump commit to a14c0be
    
    * Bump commit to 967e686
    
    * Bump commit to 379cc57
    
    * Bump commit to bcd8028
    
    * Bump commit to 0eb33ec
    
    * Bump commit to caa0220
    
    * Bump commit to 68f14d3
    
    * Bump commit to 62fc590
    
    * Bump commit to 1f0aa80
    
    * Bump commit to 1d4c6d5
    
    * Bump commit to 8e87886
    
    * Bump commit to 29a1462
    
    * Bump commit to 4fa2cc2
    
    * Bump commit to ce47d27
    
    * Bump commit to 347f216
    
    * Bump commit to 82dff89
    
    * Bump commit to 5d0bdc1
    
    * Bump commit to e1a6c24
    
    * Bump commit to 21d4b2c
    
    * Bump commit to e9f300b
    
    * Bump commit to 51f2c4d
    
    * Bump commit to bd4f6f3
    
    * Bump commit to ba6fae6
    
    * Bump commit to c1dda28
    
    * Bump commit to 2264c44
    
    * Bump commit to 1bc423d
    
    * Bump commit to fb02d59
    
    * Bump commit to c7b2293
    
    * Bump commit to e9432d3
    
    * Bump commit to bf4148a
    
    * Bump commit to d029998
    
    * Bump commit to e803b6d
    
    * Bump commit to f306e28
    
    * Bump commit to ca9c91a
    
    * Bump commit to c907f2b
    
    * Bump commit to a045d3e
    
    * Bump commit to 36bc8e0
    
    * Bump commit to aff9342
    
    * Bump commit to 4b40b9a
    
    * Bump commit to 53b044c
    
    * Bump commit to 5d3e5d9
    
    * Bump commit to 2f2c187
    
    * update script to close prs
    
    * Bump commit to 22b290c
    
    * Bump commit to f915eab
    
    * Bump commit to 7652707
    
    * Bump commit to c8f390d
    
    * Bump commit to 7fd01ac
    
    * Bump commit to 0e83367
    
    * Bump commit to 5c8c05b
    
    * Bump commit to 9f8a1d8
    
    * Bump commit to 6b7d12e
    
    * Bump commit to 168f346 (#193)
    
    * Bump commit to 69c983e
    
    * Bump commit to e0417bc
    
    * Bump commit to 59160c5
    
    * Bump commit to ec0d776
    
    * Bump commit to 5ee9697
    
    * Bump commit to 3e1e400
    
    * Bump commit to 1e35391
    
    * Bump commit to 381d5c4
    
    * Bump commit to 283ede8
    
    * Bump commit to be7140e
    
    * Bump commit to 337b043
    
    * Bump commit to 2d5f078
    
    * Bump commit to 8fe3af3
    
    * Bump commit to 83328e7
    
    * Bump commit to d752ce8
    
    * Bump commit to 3265df7
    
    * Bump commit to e6df652
    
    * Bump commit to 89b915b
    
    * Bump commit to 7ea742c
    
    * Bump commit to c71e912
    
    * Bump commit to 06eb30a
    
    * Bump commit to 4d8b623
    
    * Bump commit to 2fc7fdd
    
    * Bump commit to 4bb196c
    
    * Bump commit to bc218b4
    
    * Bump commit to 5b17ae3
    
    * Bump commit to 131812b
    
    * Bump commit to 38a7ce6
    
    * Bump commit to d568d12
    
    * Bump commit to 5d5d265
    
    * Bump commit to 7c8caba
    
    * Bump commit to d03151d
    
    * Bump commit to 63b5365
    
    * Use Qt6 for nightly builds (#246)
    
    * bump version t v2.4.2
    
    * bump version (#123)
    
    * bump version to 2.4.5 (#203)
    
    * update to 2.4.6 (#229)
    
    * use qt6 (#245)
    
    * use qt6
    
    * add qt6 build config option
    
    * update qtkeychain
    
    * fix qtkeychain build
    
    * Bump commit to b85d666
    
    * Bump commit to ccaedc3
    
    * Bump commit to b975900
    
    * Bump commit to 12808d3
    
    * Bump commit to da31e47
    
    * Bump commit to 5c0219c
    
    * Bump commit to 7ecbfa0
    
    * Bump commit to 25c776c
    
    * Bump commit to 7efe58c
    
    * Bump commit to 5634687
    
    * Bump commit to 03b32bf
    
    * Bump commit to 4e63a1b
    
    * Bump commit to 44abe6b
    
    * Bump commit to c3d3903
    
    * bump qt to 6.6 (#268)

---
+++ bump-commit.sh
@@ -0,0 +1,41 @@
+#/bin/env bash
+set -e
+
+COMMIT_YAML_PATH=".modules[3].sources[0].commit"
+
+LATEST_COMMIT=$(git ls-remote --refs https://github.com/chatterino/chatterino2/ master | awk '{print substr($1,0,7);}')
+
+COMMIT_BRANCH_NAME="bump-commit-$LATEST_COMMIT"
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$CURRENT_BRANCH" = "$COMMIT_BRANCH_NAME" ]; then
+    echo "New commit PR already exists, trying to merge"
+    gh pr merge "$COMMIT_BRANCH_NAME" -m -d
+    echo "Merged!"
+
+    gh pr list --json title,number -q '.[] | select(.title | contains("Bump commit to")) | .number' | xargs -L 1 gh pr close
+
+    git checkout branch/nightly
+else
+    git checkout branch/nightly
+    git pull
+
+    CURRENT_COMMIT=$(yq -r "$COMMIT_YAML_PATH" com.chatterino.chatterino.yml)
+
+    if [ "$CURRENT_COMMIT" = "$LATEST_COMMIT" ]; then
+        echo "Already on latest commit"
+    else
+        echo "Updating $CURRENT_COMMIT > $LATEST_COMMIT"
+
+        git checkout -b $COMMIT_BRANCH_NAME
+
+        yq -yi "$COMMIT_YAML_PATH = \"$LATEST_COMMIT\"" com.chatterino.chatterino.yml
+
+        git add com.chatterino.chatterino.yml
+        git commit -m "Bump commit to $LATEST_COMMIT"
+        git push --set-upstream origin $COMMIT_BRANCH_NAME
+
+        gh pr create -B branch/nightly -b 'This PR was automatically created by a script.' -f
+    fi
+fi
+
